9a5e45af672db9c880d2abb36243d6e7ec50bfbd
K-Maxim <m.karpenko96@mail.ru>
2023-02-01 21:12:48 +0300

    implementation of GET requests

--- framework/urls.py
+++ framework/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from framework import views
 
 urlpatterns = [
     path('create/', views.AddingData.as_view()),
+    path('frameworks/', views.DataListView.as_view()),
+    re_path('^frameworks/(?P<language>.+)/$', views.DataView.as_view()),
 ]
--- framework/views.py
+++ framework/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 
+from framework.models import FrameworkModel
 from framework.serializers import FrameworkSerializer
 
 
 class AddingData(CreateAPIView):
+    """Заполнение базы"""
     serializer_class = FrameworkSerializer
+
+
+class DataListView(ListAPIView):
+    """Список всех языков и фреймворков"""
+    serializer_class = FrameworkSerializer
+
+    def get_queryset(self):
+        return FrameworkModel.objects.all()
+
+
+class DataView(ListAPIView):
+    """Вывод данных в зависимости от фреймворка"""
+    serializer_class = FrameworkSerializer
+    lookup_field = ('language')
+
+    def get_queryset(self):
+        language = self.kwargs['language']
+        return FrameworkModel.objects.filter(language__iexact=language)
